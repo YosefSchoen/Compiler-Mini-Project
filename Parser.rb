1e0596c4acf6ba6e0a48db6cfb987e7aa9056f08
Yosef Schoen <josephaschoen@gmail.com>
2019-12-31 15:06:49 +0200

    Almost done with parser

--- Parser.rb
+++ Parser.rb
@@ -1,329 +1,274 @@
-# production rules to make XML files
-def classProduction(cmds, alphabet, keywords, otherClassesNames)
-  newCmds = []
-  newCmds.
-  str = ""
-  if cmds[0] == "class" and cmds[2] == "{" and cmds[5] == "}"
-    str =
-        "<class>"+"\n"+
-            "<keyword>" + cmds[0] + "</keyword>"+"\n"+
-            className(cmds[1],  alphabet, keywords) +
-            "<symbol>" + cmds[2] + "</symbol>"+"\n"+
-            classVarDec(cmds, alphabet, keywords, otherClassesNames) +
-            subRoutineDec(cmds, alphabet, keywords, otherClassesNames) +
-            "<symbol>" + cmds[5] + "</symbol>"+"\n"+
-        "</class>"+"\n"
-  end
+def isType(str, classNames)
+  return (str == "int" or str == "char" or str == "boolean" or classNames.include?(str))
+end
 
+
+def isSubRoutineType(str)
+  return (str == "constructor" or str == "function" or str == "method")
+end
+
+
+def getXMLString(tokens, i)
+  str = "<"+tokens[i][0]+">"+tokens[i+1][1]+"</"+tokens[i][0]+">"+"\n"
   return str
 end
 
 
-def isType(str, otherClassesNames)
-  return (str == "int" or str =="char" or str == "boolean" or otherClassesNames.include?(str))
-end
-
-def classVarDec(cmds, alphabet, keywords, otherClassesNames)
-  if (cmds[0] == "static" or cmds[0] == "field") and cmds[4] == ";"
-    str =
-        "<classVarDec>"+"\n"+
-            "<keyword>" + cmds[0] + "</keyword>"+"\n"
-            varType(cmds[1], otherClassesNames) +
-            varName(cmds[2], alphabet, keywords) +
-            classVarDecT(cmds, alphabet, keywords, 3, "")
-            "<symbol>" + cmds[4] + "</symbol>"+"\n"
-        "</classVarDec>"+"\n"
-  end
-
-  return str
-end
-
-
-def classVarDecT(cmds, alphabet, keywords, i, result)
-  if cmds[i] == ","
-    return result
-  end
-
-  result = result +
-      "<symbol>" + cmds[i] + "</symbol>"+"\n"+
-      varName(cmds[i+1], alphabet, keywords)
-
-
-   return classVarDecT(cmds, alphabet, keywords, i+1, result)
-
-  end
-
-
-def varType(str, otherClassesNames)
-  newStr = ""
-
-  if isType(str, otherClassesNames)
-    newStr = "<keyword>" + str + "</keyword>"+"\n" # int, boolean, classname
-  end
-
-  return newStr
-end
-
-
-def subRoutineDec(cmds, alphabet, keywords, otherClassesNames)
-  str = ""
-  if cmds[0] == "constructor" or "function" or "method"
-    if cmds[1] == "void" or isType(cmds[1], otherClassesNames)
-      if cmds[3] == "(" and cmds[5] == ")"
-      str =
-          "<subRoutineDec>"+"\n"+
-              "<keyword>" + cmds[0] + "</keyword>"+"\n"+
-              "<keyword>" + cmds[1] + "</keyword>"+"\n"+
-              subroutineName(cmds[2], alphabet, keywords) +
-              "<symbol>"+cmds[3]+"</symbol>"+"\n"+
-              parameterList(cmds[4], alphabet, keywords, otherClassesNames) +
-              "<symbol>"+cmds[5]+"</symbol>"+"\n"+
-              subroutineBody(cmds[6], alphabet, keywords, otherClassesNames)+
-          "</subRoutineDec>"+"\n"
-      end
-    end
-  end
-
-  return str
-end
-
-
-def parameterList(cmds, alphabet, keywords, otherClassesNames)
-  str =
-      "<parameterList>"+"\n"+
-          varType(cmds[0], otherClassesNames) +
-          varName(cmds[1], alphabet, keywords) +
-      "</parameterList>"+"\n"
-  return str
-end
-
-
-def subroutineBody(cmds, alphabet, keywords, otherClassesNames)
-  str = ""
-  if cmds[0] == "{" and cmds[3] == "}"
-    str =
-        "<subroutineBody>"+"\n"+
-            "<symbol>"+cmds[0]+"</symbol>"+"\n"+
-            varDec(cmds[1], alphabet, keywords, otherClassesNames)
-            statements(cmds[2]) +
-            "<symbol>"+cmds[3]+"</symbol>"+"\n"+
-        "<subroutineBody>"+"\n"
-  end
-
-  return str
-end
-
-
-def varDec(cmds, alphabet, keywords, otherClassesNames)
+def compileClass(tokens, alphabet, keyWords, classNames)
+  i = 0
   str = ""
 
-  if cmds[0] == "var" and cmds[3] == ";"
-    str =
-        "<varDec>"+"\n"+
-            "<keyword>" + cmds[0] + "</keyword>"+"\n"+
-            varType(cmds[1], otherClassesNames) +
-            varName(cmds[2], alphabet, keywords)
-            "<symbol>"+cmds[3]+"</symbol>"+"\n"+
-        "</varDec>"+"\n"
+  #terminal class
+  if tokens[i][1] == "class"
+    str+= "<class>"+"\n"
+    i+=1
+  end
+
+  # terminal className
+  if isIdentifier(tokens[i][1], alphabet, keyWords)
+    str+= getXMLString(tokens, i)
+    i+=1
+  end
+
+  # terminal {
+  if tokens[i][1] == "{"
+    getXMLString(tokens, i)
+    i+=1
+  end
+
+  #classVarDec*
+  resultList = compileClassVarDec(tokens, alphabet, keyWords, classNames, i)
+  str+= resultList[0]
+  i = resultList[1]
+
+  #subroutineDec*
+  resultList = compileSubroutineDec(tokens, alphabet, keyWords, classNames, i)
+  str+= resultList[0]
+  i = resultList[1]
+
+  # terminal }
+  if tokens[i][1] == "}"
+    str+= getXMLString(tokens, i)
   end
 
   return str
 end
 
 
-def className(str,  alphabet, keywords)
-  if isIdentifier(str, alphabet, keywords)
-    return str
+def compileClassVarDec(tokens, alphabet, keyWords, classNames, i)
+  resultList = compileClassVarDecT(tokens, alphabet, keyWords, classNames, i, "")
+  str = resultList[0]
+  i = resultList[1]
+
+  if tokens[i][1] == ";"
+    str+= getXMLString(tokens, i)
+    i+=1
   end
+
+  return [str, i]
 end
 
 
-def subroutineName(str, alphabet, keywords)
-  if isIdentifier(str, alphabet, keywords)
-    return str
+def compileClassVarDecT(tokens, alphabet, keyWords, classNames, i, result)
+  if tokens[i][1] != "static" or tokens[i][1] != "field"
+    return [result, i]
   end
-end
 
-
-def varName(str, alphabet, keywords)
-  if isIdentifier(str, alphabet, keywords)
-    return str
+  if tokens[i][1] == "static" or tokens[i][1] == "field"
+    result+= getXMLString(tokens, i)
+    i+=1
   end
-end
 
-def statements(stmt)
-  str = "<statements>"+stmt+"</statements>"+"\n"
-  return str
-end
-
-def statement(stmt)
-  if stmt == ""
-
+  if isType(tokens[i][1], classNames)
+    result+= getXMLString(tokens, i)
+    i+=1
   end
+
+  if isIdentifier(tokens[i][1], alphabet, keyWords)
+    result+= getXMLString(tokens, i)
+    i+=1
+  end
+
+  resultList=varNameT(tokens, alphabet, keyWords, i, result)
+  result+=resultList[0]
+  i = resultList[1]
+
+
+  resultList = compileClassVarDecT(tokens, alphabet, keyWords, classNames, i, result)
+  return resultList
 end
 
-def letStatement(stmt, alphabet, keywords)
+def varNameT(tokens, alphabet, keyWords, i, result)
+  if tokens[i][1] != ","
+    return [result, i]
+  end
+
+  if tokens[i][1] == ","
+    result+= getXMLString(tokens, i)
+    i+=1
+  end
+
+  if isIdentifier(tokens[i][1], alphabet, keyWords)
+    result+=getXMLString(tokens, i)
+    i+=1
+  end
+
+  resultList = varNameT(tokens, alphabet, keyWords, i, result)
+  return resultList
+end
+
+
+def compileSubroutineDec(tokens, alphabet, keyWords, classNames, i)
   str = ""
-  if stmt[0] == "let"  and stmt[3] == "=" and stmt[5] == ";"
-    str =
-        "<letStatement>"+"\n"+
-            "<keyword>" + stmt[0] + "</keyword>"+"\n"+
-            "<identifier>" + varName(stmt[1], alphabet, keywords) + "</identifier"+"\n"+
-            expression(stmt[2])+
-            "<symbol>" + stmt[3] + "</symbol"+"\n"+
-            expression(stmt[4])+
-        "</letStatement>"+"\n"
+  if isSubRoutineType(tokens[i][1])
+    str+= getXMLString(tokens, i)
+    i+=1
   end
 
-end
-
-def ifStatement(stmt)
-  if stmt[0] == "if" and stmt[1] == "(" and stmt[3] == ")" and stmt[4] == "{" and stmt[6] ==  "}"
-    str =
-        "<ifStatement>"+"\n"+
-            "<keyword>" + stmt[0] + "</keyword>"+"\n"+
-            "<symbol>" + stmt[1] + "</symbol"+"\n"+
-            expression(stmt[2])+
-            "<symbol>" + stmt[3] + "</symbol"+"\n"+
-            "<symbol>" + stmt[4] + "</symbol"+"\n"+
-            statements(stmt[5])+
-            "<symbol>" + stmt[6] + "</symbol"+"\n"+
-            if stmt[7] == "else"
-              elseStatement(stmt[7])
-            end
-    str =
-        str + "</ifStatement>"+"\n"
+  if isType(tokens[i][1], classNames) or tokens[i][1] == "void"
+    str+= getXMLString(tokens, i)
+    i+=1
   end
 
-  return str
-end
-
-def elseStatement(stmt)
-  str = ''
-  if stmt[0] == "else" and stmt[1] == "{" and stmt[3] == "}"
-    str =
-        "<keyword>" + stmt[0] + "</keyword>"+"\n"+
-        "<symbol>" + stmt[1] + "</symbol"+"\n"+
-        statements(stmt[2])+
-        "<symbol>" + stmt[3] + "</symbol"+"\n"
+  if isIdentifier(tokens[i][1], alphabet, keyWords)
+    str+= getXMLString(tokens, i)
+    i+=1
   end
 
-  return str
-end
-
-
-def whileStatement(stmt)
-  if stmt[0] == "while" and stmt[1] == "(" and stmt[3] == ")" and stmt[4] == "{" and stmt[6] ==  "}"
-    str =
-        "<whileStatement>"+"\n"+
-            "<keyword>" + stmt[0] + "</keyword>"+"\n"+
-            "<symbol>" + stmt[1] + "</symbol"+"\n"+
-            expression(stmt[2])+
-            "<symbol>" + stmt[3] + "</symbol"+"\n"+
-            "<symbol>" + stmt[4] + "</symbol"+"\n"+
-            statements(stmt[5])+
-            "<symbol>" + stmt[6] + "</symbol"+"\n"+
-        "</whileStatement>"+"\n"
-
+  if tokens[i][1] == "("
+    str+= getXMLString(tokens, i)
+    i+=1
   end
-  return str
-end
 
+  resultList = compileParameterList(tokens, alphabet, keyWords, classNames, i)
+  str += resultList[0]
+  i = resultList[1]
 
-def doStatement(stmt)
-  if stmt[0] == "do" and stmt[2] == ";"
-    str =
-        "<doStatement"+"\n"+
-            "<keyword>" + stmt[0] + "</keyword>"+"\n"+
-            subroutineCall(stmt[1])+
-            "<symbol>" + stmt[2] + "</symbol"+"\n"+
-        "</doStatement>"+"\n"
+  if tokens[i][1] == ")"
+    str+= getXMLString(tokens, i)
+    i+=1
   end
+
+  resultList = compileSubroutineBody(tokens, alphabet, keyWords, classNames, i)
+  str += resultList[0]
+  i += resultList[1]
+
+  return [str, i]
 end
 
 
-def ReturnStatement(stmt)
-  if stmt[0] == "return" and stmt[2] == ";"
-    str =
-        "<ReturnStatement"+"\n"+
-            "<keyword>" + stmt[0] + "</keyword>"+"\n"+
-            expression(stmt[1]) +
-            "<symbol>" + stmt[2] + "</symbol>"+"\n"+
-        "</ReturnStatement>"+"\n"
-  end
-  return str
-end
-
-
-def expression(cmds)
-
-end
-
-
-def term(cmds, alphabet, keyWords, keyWordsConstant)
-
-
-  str = "<term>"+"\n"
-  case cmds
-  when isIntConstant(cmds)
-    str = str + "<integerConstant>" + cmds + "</integerConstant>"+"\n"
-  when isStringConstant(cmds)
-    str = str + "<StringConstant>" + cmds + "</StringConstant>"+"\n"
-  when boolKeywordConstant(cmds, keyWordsConstant)
-    str = str + KeywordConstant(cmds, keyWordsConstant)
-  when isIdentifier(cmds, alphabet, keyWords)
-
-
-
-  end
-  str = str + "</term>"+"\n"
-  return str
-end
-
-
-def subroutineCall(cmds)
-
-end
-
-
-def expressionList(cmds)
-
-end
-
-
-def op(op, ops)
+def compileSubroutineBody(tokens, alphabet, keyWords, classNames, i)
   str = ""
-  if ops.include?(op)
-    str = "<symbol>"+op+"</symbol>"+"\n"
+
+  if tokens[i][0] == "{"
+    str+= getXMLString(tokens, i)
+    i+=1
   end
 
-  return str
+  resultList = compileVarDec(tokens, alphabet, keyWords, classNames, i)
+  str += resultList[0]
+  i = resultList[1]
+
+  if tokens[i][0] == "}"
+    str+= getXMLString(tokens, i)
+    i+=1
+  end
+
+  return [str, i]
 end
 
 
-def unaryOp(op, ops)
+def compileParameterList(tokens, alphabet, keyWords, classNames, i)
+  resultList = compileParameterListT(tokens, alphabet, keyWords, classNames, i, "")
+  return resultList
+end
+
+def compileParameterListT(tokens, alphabet, keyWords, classNames, i, result)
+  unless isType(tokens[i][0], classNames)
+    return [result, i]
+  end
+
+  if isType(tokens[i][0], classNames)
+    result+= getXMLString(tokens, i)
+    i+=1
+  end
+
+  if isIdentifier(tokens[i][1], alphabet, keyWords)
+    result+=getXMLString(tokens, i)
+    i+=1
+  end
+
+  result+= varNameT(tokens, alphabet, keyWords, i, result)
+
+  return [result, i]
+end
+
+
+def compileVarDec(tokens, alphabet, keyWords, classNames, i)
+
   str = ""
-  if ops.include?(op)
-    str = "<symbol>"+op+"</symbol>"+"\n"
+  if tokens[i][1] == "var"
+    str+= getXMLString(tokens, i)
+    i+=1
   end
 
-  return str
+  if isType(tokens[i][1], classNames)
+    str+= getXMLString(tokens, i)
+    i+=1
+  end
+
+  resultList = varNameT(tokens, alphabet, keyWords, i, "")
+  str+= resultList[0]
+  i = resultList[1]
+
+  if tokens[i][1] == ";"
+    str+=getXMLString(tokens, i)
+    i+=1
+  end
+
+  return [str, i]
 end
 
-# return the keywords const
-def KeywordConstant(keyConst, keyConstants)
-  str = ""
-  if keyConstants.include?(keyConst)
-    str = "<keyword>"+keyConst+"</keyword>"+"\n"
-  end
+def compileStatements(tokens, alphabet, keyWords, i)
 
-  return str
 end
 
-# check if keyword is a constant
-def boolKeywordConstant(keyConst, keyConstants)
-  if keyConst == "true" or keyConst == "false" or keyConst == "null" or keyConst == "this"
-    return true
-  end
-  return false
+
+def compileDo(tokens, alphabet, keyWords, i)
+
+end
+
+
+def compileLet(tokens, alphabet, keyWords, i)
+
+end
+
+
+def compileWhile(tokens, alphabet, keyWords, i)
+
+end
+
+
+def compileReturn(tokens, alphabet, keyWords, i)
+
+end
+
+
+def compileIf(tokens, alphabet, keyWords, i)
+
+end
+
+
+def compileExpression(tokens, alphabet, keyWords, i)
+
+end
+
+
+def compileTerm(tokens, alphabet, keyWords, i)
+
+end
+
+
+def compileExpressionList(tokens, alphabet, keyWords, i)
+
 end
