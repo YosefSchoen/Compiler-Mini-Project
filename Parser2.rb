2dc47542acfe9562b76c3b0a96ea203d3e98a6ce
SaulYourFriend <baseballsaul@gmail.com>
2019-12-31 11:41:28 +0200

    need to fix helpers

---
+++ Parser2.rb
@@ -0,0 +1,44 @@
+#tokens is of form [type, string]
+
+
+
+def structure(tokens)
+  tokens.each do |token|
+    type = token[0]
+    str = token[1]
+    case type
+
+    end
+  end
+
+
+end
+
+def isClass(tokens)
+
+end
+
+def KeywordConstant
+  str = ""
+  if cmds == "true" or cmds == "false" or cmds == "null" or cmds == "this"
+    str += cmds
+    return str
+  end
+  return false
+end
+
+
+def statemnent(cmds)
+  case cmds
+  when letStatement
+
+  when ifStatement
+
+  when whileStatement
+
+  when doStatement
+
+  when returnStatement
+  end
+
+end
