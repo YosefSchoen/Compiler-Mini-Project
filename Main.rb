fec9980a45bd236665ab131fb0523c07ec763d0c
Yosef Schoen <josephaschoen@gmail.com>
2020-01-23 18:38:18 +0200

    Maybe needs some debug

--- Main.rb
+++ Main.rb
@@ -7,30 +7,30 @@ require_relative 'Compiler/CompilerIO'
 
 #the main will translate all of the test cases from projects 7 and 8
 # and compile the test cases from projects 9, 10, and 11
-def main
+def main(path)
 
   translateVmToHack("TestFiles/Project7/MyTest", "TestFiles/Project7/MyTest/test.asm")
-  #translateVmToHack("TestFiles/Project7/Stage1/SimpleAdd", "TestFiles/Project7/Stage1/SimpleAdd/SimpleAdd.asm")
-  #translateVmToHack("TestFiles/Project7/Stage1/StackTest", "TestFiles/Project7/Stage1/StackTest/StackTest.asm")
+  translateVmToHack("TestFiles/Project7/Stage1/SimpleAdd", "TestFiles/Project7/Stage1/SimpleAdd/SimpleAdd.asm")
+  translateVmToHack("TestFiles/Project7/Stage1/StackTest", "TestFiles/Project7/Stage1/StackTest/StackTest.asm")
 
-  #translateVmToHack("TestFiles/Project7/Stage2/BasicTest", "TestFiles/Project7/Stage2/BasicTest/BasicTest.asm")
-  #translateVmToHack("TestFiles/Project7/Stage2/PointerTest", "TestFiles/Project7/Stage2/PointerTest/PointerTest.asm")
-  #translateVmToHack("TestFiles/Project7/Stage2/StaticTest", "TestFiles/Project7/Stage2/StaticTest/StaticTest.asm")
+  translateVmToHack("TestFiles/Project7/Stage2/BasicTest", "TestFiles/Project7/Stage2/BasicTest/BasicTest.asm")
+  translateVmToHack("TestFiles/Project7/Stage2/PointerTest", "TestFiles/Project7/Stage2/PointerTest/PointerTest.asm")
+  translateVmToHack("TestFiles/Project7/Stage2/StaticTest", "TestFiles/Project7/Stage2/StaticTest/StaticTest.asm")
 
-  #translateVmToHack("TestFiles/Project8/ProgramFlow/BasicLoop", "TestFiles/Project8/ProgramFlow/BasicLoop/BasicLoop.asm")
-  #translateVmToHack("TestFiles/Project8/ProgramFlow/FibonacciSeries", "TestFiles/Project8/ProgramFlow/FibonacciSeries/FibonacciSeries.asm")
+  translateVmToHack("TestFiles/Project8/ProgramFlow/BasicLoop", "TestFiles/Project8/ProgramFlow/BasicLoop/BasicLoop.asm")
+  translateVmToHack("TestFiles/Project8/ProgramFlow/FibonacciSeries", "TestFiles/Project8/ProgramFlow/FibonacciSeries/FibonacciSeries.asm")
 
-  #translateVmToHack("TestFiles/Project8/FunctionCalls/SimpleFunction", "TestFiles/Project8/FunctionCalls/SimpleFunction/SimpleFunction.asm")
-  #translateVmToHack("TestFiles/Project8/FunctionCalls/FibonacciElement", "TestFiles/Project8/FunctionCalls/FibonacciElement/FibonacciElement.asm")
-  #translateVmToHack("TestFiles/Project8/FunctionCalls/NestedCall", "TestFiles/Project8/FunctionCalls/NestedCall/NestedCall.asm")
-  #translateVmToHack("TestFiles/Project8/FunctionCalls/StaticsTest", "TestFiles/Project8/FunctionCalls/StaticsTest/StaticsTest.asm")
+  translateVmToHack("TestFiles/Project8/FunctionCalls/SimpleFunction", "TestFiles/Project8/FunctionCalls/SimpleFunction/SimpleFunction.asm")
+  translateVmToHack("TestFiles/Project8/FunctionCalls/FibonacciElement", "TestFiles/Project8/FunctionCalls/FibonacciElement/FibonacciElement.asm")
+  translateVmToHack("TestFiles/Project8/FunctionCalls/NestedCall", "TestFiles/Project8/FunctionCalls/NestedCall/NestedCall.asm")
+  translateVmToHack("TestFiles/Project8/FunctionCalls/StaticsTest", "TestFiles/Project8/FunctionCalls/StaticsTest/StaticsTest.asm")
 
 
-  #compile("TestFiles/Project9")
-  #compile("TestFiles/Project10/MyTest")
-  #compile("TestFiles/Project10/ExpressionLessSquare")
-  #compile("TestFiles/Project10/Square")
-  #compile("TestFiles/Project10/ArrayTest")
+  compile("TestFiles/Project9")
+  compile("TestFiles/Project10/MyTest")
+  compile("TestFiles/Project10/ExpressionLessSquare")
+  compile("TestFiles/Project10/Square")
+  compile("TestFiles/Project10/ArrayTest")
 
   compile("TestFiles/Project11/Seven")
   compile("TestFiles/Project11/ConvertToBin")
@@ -40,7 +40,9 @@ def main
   compile("TestFiles/Project11/ComplexArrays")
 
 
+  #compile(path)
+  #translateVmToHack(path, path+"/asmFile")
 end
 
-
-main
+path = ""
+main(path)
